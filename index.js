924ba89c02648a9d5a023783a891ddd7ffaf93ec
Ricard <rikitzzz@gmail.com>
2019-04-23 00:38:13 +0200

    jQuery
    Using jQuery to add data to the target div
    Added styles for the classes of the elements added during the api query

--- index.js
+++ index.js
@@ -1,11 +1,67 @@
 function getEspecialitats() {
-	console.log("Getting especialitats");
-	var collapseEspecialitats = document.getElementById("collapseEspecialitats");
-	var bodyEspecialitats = document.getElementById("bodyEspecialitats");
 
-	if(!collapseEspecialitats.classList.contains("show")){
+
+	console.log("Getting especialitats");
+	const collapseEspecialitats = document.getElementById("collapseEspecialitats");
+	const bodyEspecialitats = document.getElementById("bodyEspecialitats");
+	const isLoaded = $("#bodyEspecialitats").data('is-loaded');
+	console.log(`Is loaded? ${isLoaded}`);
+
+	if (!collapseEspecialitats.classList.contains("show") && !isLoaded) {
 		console.log("Querying data...");
-		bodyEspecialitats.innerHTML = "Get Element By Id";
+		$("#bodyEspecialitats").html("Carregant");
+		getResponseJSON("#bodyEspecialitats");
 	}
 
+}
+
+async function getResponseJSON(htmlElement) {
+	var interval = setInterval(loadingData.bind(null, $(htmlElement)), 250);
+	const response = await $.getJSON(wordnikApiRequest())
+		.complete(function (data) {
+			console.log("Complete!");
+			clearInterval(interval);
+			addFormattedData(htmlElement, data.responseJSON);
+			$(htmlElement).data('is-loaded', true);
+		});
+}
+
+function loadingData(htmlElement) {
+	var htmlString = $(htmlElement).html();
+	var dots = htmlString.replace("Carregant", "");
+	if (dots.length % 4 >= 3) {
+		htmlString = "Carregant";
+	} else {
+		htmlString += ".";
+	}
+	$(htmlElement).html(htmlString);
+}
+
+function addFormattedData(targetElement, data) {
+	var s = "";
+	for (let i = 0; i < data.length; i++) {
+		for (let j in data[i]) {
+			s += `<h5 class="specialty-name">${j}</h5>`;
+			if (Array.isArray(data[i][j])) {
+				s += "<ul>";
+				for (let k = 0; k < data[i][j].length; k++) {
+					s += `<li class="specialty-doctor">${data[i][j][k]}</li>`;
+				}
+				s += "</ul>";
+			} else {
+				s += `<p class="specialty-doctor">${data[i][j]}</p>`;
+			}
+		}
+		s += "<hr>";
+	}
+	$(targetElement).html(s);
+}
+
+function wordnikApiRequest() {
+	var url = "https://api.wordnik.com/v4/word.json/"
+	var mode = "relatedWords";
+	var apiKey = "c23b746d074135dc9500c0a61300a3cb7647e53ec2b9b658e";
+	var data = "useCanonical=false&limitPerRelationshipType=10&api_key=" + apiKey;
+
+	return `${url}happy/${mode}?${data}`;
 }
