52d860bc3aeac7c9a332155682f65b13163be630
Ricard <rikitzzz@gmail.com>
2019-04-23 13:34:09 +0200

    Trying to access a webservice from CMS

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ function getEspecialitats() {
 
 async function getResponseJSON(htmlElement) {
 	var interval = setInterval(loadingData.bind(null, $(htmlElement)), 125);
-	const response = await $.getJSON(wordnikApiRequest())
+	const response = await $.getJSON(apiRequest())
 		.complete(function (data) {
 			console.log("Complete!");
 			clearInterval(interval);
 			addFormattedData(htmlElement, data.responseJSON);
-			$(htmlElement).data('is-loaded', true);
+			// $(htmlElement).data('is-loaded', true);
 		});
 }
 
@@ -62,11 +62,57 @@ function arrayAsUnorderedList(array) {
 	return s;
 }
 
-function wordnikApiRequest() {
+function apiRequest() {
 	var url = "https://api.wordnik.com/v4/word.json/"
 	var mode = "relatedWords";
 	var apiKey = "c23b746d074135dc9500c0a61300a3cb7647e53ec2b9b658e";
 	var data = "useCanonical=false&limitPerRelationshipType=10&api_key=" + apiKey;
 
 	return `${url}happy/${mode}?${data}`;
+}
+
+function construitxml(nNombre1, nNombre2) {
+	var f = createDummyForm();
+
+	var requete;
+	requete = "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsi=\"http://www.w3.org/1999/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/1999/XMLSchema\"><soap:Body>";
+
+	requete = requete + "<nNombre1 xsd:type=\"xsd:int\" xmlns=\"urn:Webservice_CMS\">";
+	requete = requete + nNombre1;
+	requete = requete + "</nNombre1>";
+
+	requete = requete + "<nNombre2 xsd:type=\"xsd:int\" xmlns=\"urn:Webservice_CMS\">";
+	requete = requete + nNombre2;
+	requete = requete + "</nNombre2>";
+
+	requete = requete + "</soap:Body></soap:Envelope>";
+	xmlInput.value = requete;
+	actionInput.value = "urn:Webservice_CMS/Addition";
+	document.body.appendChild(f);
+	
+	f.submit();
+}
+
+function createDummyForm(){
+	document.createElement("form");
+	f.setAttribute('action', "http://dell2900/CENTREMEDIC_WSTEST_WEB/awws/Webservice_CMS.awws");
+	f.setAttribute('method', "post");
+	f.setAttribute('name', "xmlform");
+	f.setAttribute('id', "xmlform");
+	f.setAttribute('target', "_self");
+
+	var xmlInput = document.createElement("input"); //input element, text
+	xmlInput.setAttribute('type', "hidden");
+	xmlInput.setAttribute('name', "xml");
+	xmlInput.setAttribute('id', "xml");
+
+	var actionInput = document.createElement("input"); //input element, text
+	actionInput.setAttribute('type', "hidden");
+	actionInput.setAttribute('name', "action");
+	actionInput.setAttribute('id', "action");
+
+	f.appendChild(xmlInput);
+	f.appendChild(actionInput);
+
+	return f;
 }
