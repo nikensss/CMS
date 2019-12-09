63b8975d6bceb05bafd7a58dcea2788cfc34fa2e
nikensss <rikitzzz@gmail.com>
2019-12-10 07:35:38 +0800

    Small formatting changes

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function requestEspecialitats() {
-	const collapseEspecialitats = document.getElementById("especialitats");
+  const collapseEspecialitats = document.getElementById("especialitats");
 
-	if (!collapseEspecialitats.classList.contains("show")) {
-		console.log("Mostrant especialitats!");
-		removeChildren(collapseEspecialitats);
-		const i = document.createElement("iframe");
-		i.setAttribute('id', 'response');
-		i.setAttribute('name', 'response');
-		i.setAttribute('src', 'http://servamed1.com/pcsc_web/L45/page_frontal_demanavisita.awp');
-		i.classList.add("w-100");
-		collapseEspecialitats.appendChild(i);
-	}
+  if (!collapseEspecialitats.classList.contains("show")) {
+    console.log("Mostrant especialitats!");
+    removeChildren(collapseEspecialitats);
+    const i = document.createElement("iframe");
+    i.setAttribute('id', 'response');
+    i.setAttribute('name', 'response');
+    i.setAttribute('src', 'http://servamed1.com/pcsc_web/L45/page_frontal_demanavisita.awp');
+    i.classList.add("w-100");
+    collapseEspecialitats.appendChild(i);
+  }
 }
 
 function removeChildren(htmlElement) {
-	while (htmlElement.firstChild) {
-		htmlElement.removeChild(htmlElement.firstChild);
-	}
+  while (htmlElement.firstChild) {
+    htmlElement.removeChild(htmlElement.firstChild);
+  }
 }
