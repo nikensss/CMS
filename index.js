ccfde2800ba8bb9934e73849d1130219efad2f7f
nikensss <rikitzzz@gmail.com>
2019-12-10 07:08:04 +0800

    Removed all uses of id in css

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 function requestEspecialitats() {
-	const collapseEspecialitats = document.getElementById("collapseEspecialitats");
-	const bodyEspecialitats = document.getElementById("bodyEspecialitats");
+	const collapseEspecialitats = document.getElementById("especialitats");
 
 	if (!collapseEspecialitats.classList.contains("show")) {
 		console.log("Mostrant especialitats!");
-		removeChildren(bodyEspecialitats);
+		removeChildren(collapseEspecialitats);
 		const i = document.createElement("iframe");
 		i.setAttribute('id', 'response');
 		i.setAttribute('name', 'response');
-		i.setAttribute('style', 'display:block;');
 		i.setAttribute('src', 'http://servamed1.com/pcsc_web/L45/page_frontal_demanavisita.awp');
-		bodyEspecialitats.appendChild(i);
+		i.classList.add("w-100");
+		collapseEspecialitats.appendChild(i);
 	}
 }
 
