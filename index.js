48349f954edc238e6146c7a24d8cb2e7b10b23cf
nikensss <rikitzzz@gmail.com>
2019-12-22 19:56:05 +0100

    Simplified code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function requestEspecialitats() {
-  const collapseEspecialitats = document.getElementById("especialitats");
+  const collapseEspecialitats = document.getElementById('especialitats');
 
-  if (!collapseEspecialitats.classList.contains("show")) {
-    console.log("Mostrant especialitats!");
+  if (!collapseEspecialitats.classList.contains('show')) {
+    console.log('Mostrant especialitats!');
     removeChildren(collapseEspecialitats);
-    const i = document.createElement("iframe");
+    const i = document.createElement('iframe');
     i.setAttribute('id', 'response');
     i.setAttribute('name', 'response');
     i.setAttribute('src', 'http://servamed1.com/pcsc_web/L45/page_frontal_demanavisita.awp');
-    i.classList.add("w-100");
+    i.classList.add('w-100');
     collapseEspecialitats.appendChild(i);
   }
 }
